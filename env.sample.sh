7d01213b11e031302995fe6d5d98069e1e21bb83
Lucas N Martins <lucasmartins@railsnapraia.com>
2014-01-27 13:24:24 -0200

    updating env.sh generator to set user/passwd

--- env.sample.sh
+++ env.sample.sh
@@ -1,7 +1,9 @@
 # you can run:
 # $ . ./env.sample.sh
-# to use these variables.
+# to create an env.sh executable.
 export REDIS_URL='redis://localhost:6379'
+export MAILCANNON_MONITOR_USERNAME=''
+export MAILCANNON_MONITOR_PASSWORD=''
 
 cat env.sample.sh | grep "export\s" > env.sh
 chmod +x env.sh
